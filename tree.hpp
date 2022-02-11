29cb2c164555791ff282eb6ba19858a71f582ca6
Udith Kumar <udithkumarvd@gmail.com>
2022-02-11 11:04:40 +0530

    feat(function): add implementation of function execution

--- tree.hpp
+++ tree.hpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector>
+#include "context.hpp"
+#include "funcdef.hpp"
 #define NODE_STATEMENTS 2356534
 #define NODE_IF 572456
 #define NODE_IFELSE 574655
@@ -14,8 +17,15 @@
 #define NODE_COMP 236474
 #define NODE_ID 8902783
 #define NODE_VAL 489392
+#define NODE_FUNC 908987
+#define NODE_PARAM 908122
+#define NODE_CALL 909824
+#define NODE_ARGS 8787878
+
 using namespace std;
-extern unordered_map<string, double> exec_context;
+
+extern get_exec_context exec_context;
+extern unordered_map<string,func_def> func_table;
 class treenode{
   public:
       treenode *first,*second,*third;
@@ -28,10 +38,11 @@ class treenode{
         second = _second;
         third = _third;
       }
-      treenode(int code, string _symbol, treenode* _first = NULL){
+      treenode(int code, string _symbol, treenode* _first = NULL,treenode* _second = NULL){
         type = code;
         symbol = _symbol;
         first = _first;
+        second = _second;
       }
       treenode(int code, double val){
         type = code;
@@ -40,8 +51,43 @@ class treenode{
         second = NULL;
         third = NULL;
       }
-      
+
+      void helperlist(vector<double> & li){
+        li.push_back(first->execute());
+        if(second!=NULL){
+          second->helperlist(li);
+        }
+      }
+
+      vector<double> list(){
+        vector<double> sol;
+        sol.push_back(first->execute());
+        if(second!=NULL){
+          second->helperlist(sol);
+        }
+        return sol;
+      }
+
+      void helperplist(vector<string> &li){
+        li.push_back(first->symbol);
+        if(second!=NULL){
+          second->helperplist(li);
+        }
+      }
+
+      vector<string> plist(){
+        vector<string> sol;
+        // cout<<"plist"<<endl;
+        sol.push_back(first->symbol);
+        // cout<<"first->first->symbol"<<endl;
+        if(second!=NULL){
+          second->helperplist(sol);
+        }
+        return sol;
+      }
+
       int execute(){
+        vector<double> argslis;
         switch(type){
           case NODE_STATEMENTS:
             first->execute();
@@ -67,18 +113,22 @@ class treenode{
             break;
           case NODE_ASSIGN:
             if(exec_context.count(symbol)!=0)
-              exec_context[symbol] = first->execute();
-            return exec_context[symbol];
+              exec_context.set(symbol, first->execute());
+            return exec_context.get(symbol);
             break;
           case NODE_DECLARE:
             if(exec_context.count(symbol)==0)
-              exec_context[symbol] = 0;
+              exec_context.set(symbol,0);
             break;
           case NODE_EXPR:
             return first->execute();
             break;
           case NODE_PRINT:
+          if(symbol.empty())
             cout << first->execute() << endl;
+          else {
+            cout <<  
+          }
             break;
           case NODE_OPER:
             if(second->symbol=="+"){
@@ -115,10 +165,52 @@ class treenode{
             }
             break;
           case NODE_ID:
-            return exec_context[symbol];
+            return exec_context.get(symbol);
             break;
           case NODE_VAL:
             return value;
+            break;
+          case NODE_CALL:
+            // if(first==NULL){
+              // cout<<"tre.hpp 165"<<endl;
+              double retval;
+              if(first!=NULL){
+                for(auto x:first->list()){
+                  argslis.push_back(x);
+                }
+              }
+              // cout<<"tre.hpp 172"<<endl;
+              exec_context.push();
+              // cout<<"tre.hpp 175"<<endl;
+              if(func_table.count(symbol)!=0){
+                vector<string> varnames = func_table[symbol].args_list;
+                if(varnames.size()==argslis.size()){
+                  for(int i=0;i<varnames.size();i++){
+                    exec_context.set(varnames[i],argslis[i]);
+                  }
+                }
+                else{
+                  cout << "Error: Function " << symbol << " takes " << varnames.size() << " arguments, but " << argslis.size() << " were given." << endl;
+                }
+                retval = ((treenode*) func_table[symbol].func_def_tree)->execute();
+              }
+              exec_context.pop();
+            // }
+            break;
+          case NODE_FUNC:
+            // symbol, first is args, second is body
+            func_table[symbol].func_def_tree = second;
+            // cout<<"tre.hpp 192"<<endl;
+            if(first!=NULL)
+            func_table[symbol].args_list = first->plist();
+            else func_table[symbol].args_list = vector<string> ();
+            break;
+          case NODE_ARGS:
+            first->execute();
+            if(second!=NULL){
+              second->execute();
+            }
+            break;
         }
       }
 
